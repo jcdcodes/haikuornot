ab95f207f9144fd7cde5942ca7701b193e324bcc
Josh Daghlian <jdaghlian@JCD-MacBook-Al-3.local>
2010-01-11 23:24:56 -0500

    compojure and swank available from the maven repl

--- src/main/clojure/app.clj
+++ src/main/clojure/app.clj
@@ -1,7 +1,11 @@
 (ns app
+  (:use swank.swank)
   (:gen-class))
-
-(println "THIS IS THE APP")
 
 (defn main [& args]
   (println "This is the main function for app."))
+
+
+(defn start-swank
+  []
+  (swank.swank/start-server "/dev/null" :port 4005))
--- src/main/clojure/org/joshd/HaikuWeb.clj
+++ src/main/clojure/org/joshd/HaikuWeb.clj
@@ -3,10 +3,33 @@
   (:gen-class
    :extends javax.servlet.http.HttpServlet))
 
+(defn greet
+  [name]
+  (html
+    [:h1 "Hi again, " name]
+    [:p "Isn't this a nicely metered poem, " name "?"]
+    [:p "(Well, isn't it?)"]
+    ))
+
+(defn about
+  [params]
+  (html
+   [:h1 "About Josh's crazy Haiku generator"]
+   [:p "There's nothing all that fabulous about the Haiku generator yet.  There are pieces that run,
+but mostly it's just a bunch of maven and clojure and compojure and slime and swank configuration that
+(astonishingly!) seems to work together reasonably well.  This text is an experiment to see whether
+we can modify " [:tt "defroutes "] "blocks as easily as we modify plain old methods to which the
+routes delegate."]
+   [:p "(Can you tell I'm pretty pleased?)"]
+   [:p "Ignoring " (count params) " parameters."]))
+
 (defroutes haiku-web
   (GET "/foo/bar*" (html [:h1 "bar bar bar bar bar/bar bar bar bar bar bar bar/bar bar bar bar bar"]))
+  (GET "/foo/:name" (greet (params :name)))
   (GET "/foo*" (html [:h1 "FOOOOOOOOOOOOO"]))
+  (GET "/about*" (about params))
   (ANY "*" (html [:h1 "Visualize org.joshd.HaikuWeb in 5-7-5."]))
 )
 
 (defservice haiku-web)
+
